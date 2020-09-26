c8b57c772ad28cfb1dc49542046e9e1776d5966f
Deni Pramulia <denipramulia97@gmail.com>
2020-09-26 17:53:32 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("4844ee89-f8e6-402b-8029-4fded721b65c");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://40.118.231.181:3001";
 
